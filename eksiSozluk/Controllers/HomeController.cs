edffce1ccf492fdac1a81c0ad8438d0d250699a2
bukettgenc <bktgnc06@gmail.com>
2021-04-13 00:53:25 +0300

    yeni commit

--- eksiSozluk/Controllers/HomeController.cs
+++ eksiSozluk/Controllers/HomeController.cs
@@ -19,13 +19,12 @@ namespace eksiSozluk.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your cooontact page.";
 
             return View();
         }
