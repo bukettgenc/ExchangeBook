45cab550a5e516eafe777cf70cf8d5ec5d1026ef
bukettgenc <bktgnc06@gmail.com>
2021-04-14 18:37:49 +0300

    db

---
+++ Database/dbo/Tables/HomePages.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[HomePages] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [Post]       NVARCHAR (MAX) NULL,
+    [HomePageId] INT            NOT NULL,
+    CONSTRAINT [PK_HomePages] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Database/dbo/Tables/Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserId]     INT            IDENTITY (1, 1) NOT NULL,
+    [UserName]   NVARCHAR (MAX) NULL,
+    [Password]   NVARCHAR (MAX) NULL,
+    [Image]      NVARCHAR (MAX) NULL,
+    [HomePageId] INT            NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
