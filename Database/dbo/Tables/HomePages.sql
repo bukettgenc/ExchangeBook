3a246ac25508176da4e271670fdb26e6c025d98d
buket genç <bktgnc98@hotmail.com>
2021-04-19 14:45:15 +0000

    Deleted Database

--- Database/dbo/Tables/HomePages.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[HomePages] (
-    [Id]         INT            IDENTITY (1, 1) NOT NULL,
-    [Post]       NVARCHAR (MAX) NULL,
-    [HomePageId] INT            NOT NULL,
-    CONSTRAINT [PK_HomePages] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
